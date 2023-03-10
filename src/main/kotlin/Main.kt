c9c04eb319b48c56c7206f83948fcfbe6dc4e8ae
AmrElwkeel <amrgamalelwekeel@gmail.com>
2023-03-10 15:22:11 +0200

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+    println("Hello World!");
+    var o :Int;
+    var oo = 555.2;
+    val name = "Ahmed";
+    val names:String ;
+    val v:String ;
+    v = " ssss";
+    println(v);
+
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}");
+}
